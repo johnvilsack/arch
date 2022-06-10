2829b6023714857e965a5c417b8e1f5cee5069fa
John Vilsack <vilsack@gmail.com>
2022-06-10 15:30:38 -0500

    4

--- get.sh
+++ get.sh
@@ -1,4 +1,4 @@
-# curl -LJO https://raw.githubusercontent.com/johnvilsack/arch/master/install.sh | ./install.sh
+# curl -LJO https://raw.githubusercontent.com/johnvilsack/arch/master/get.sh && chmod 700 get.sh && ./get.sh
 
 curl -LJO https://raw.githubusercontent.com/johnvilsack/arch/master/pacman.sh
 curl -LJO https://raw.githubusercontent.com/johnvilsack/arch/master/diskbuild.sh
