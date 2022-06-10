5c63a4485a1868e3096fafb5720cf680fab79cc4
John Vilsack <vilsack@gmail.com>
2022-06-10 14:43:07 -0500

    1

--- archbuild.sh
+++ archbuild.sh
@@ -1,6 +1,6 @@
 sed -i '/#ParallelDownloads = 5/s/^#//g' /etc/pacman.conf
 pacman -Syy
-pacman -S reflector
+pacman -S --noconfirm reflector
 reflector --verbose -l 10 -f 5 -c US -p https --save /etc/pacman.d/mirrorlist
 
 fdisk -l
@@ -18,10 +18,9 @@ btrfs subvolume create /mnt/@vms
 btrfs subvolume create /mnt/@.snapshots
 umount /mnt
 mount -a rw,noatime,nodiratime,space_cache,ssd,commit=120,compress=zstd,subvol=@ /dev/nvme0n1p2 /mnt
+mount /dev/nvme0n1p1 /mnt/boot
 mkdir -p /mnt/{boot,home,vms,.snapshots,btrfs}
-rw
 mount -a rw,noatime,nodiratime,space_cache,ssd,commit=120,compress=zstd,subvol=@home /dev/nvme0n1p2 /mnt/home
 mount -a rw,noatime,nodiratime,space_cache,ssd,commit=120,compress=zstd,subvol=@vms /dev/nvme0n1p2 /mnt/vms
 mount -a rw,noatime,nodiratime,space_cache,ssd,commit=120,compress=zstd,subvol=@.snapshots /dev/nvme0n1p2 /mnt/.snapshots
 mount -a rw,noatime,nodiratime,space_cache,ssd,commit=120,compress=zstd,subvolid=5 /dev/nvme0n1p2 /mnt/btrfs
-mount /dev/nvme0n1p1 /mnt/boot
