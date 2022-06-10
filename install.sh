e3d2580ffd9b19fa324fae4ec3eecaf721d8706c
John Vilsack <vilsack@gmail.com>
2022-06-10 15:12:51 -0500

    2

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+# curl -LJO https://archinstall.vil.io
+
 sed -i '/#ParallelDownloads = 5/s/^#//g' /etc/pacman.conf
 pacman -Syy
 pacman -S --noconfirm reflector
@@ -18,8 +20,8 @@ btrfs subvolume create /mnt/@vms
 btrfs subvolume create /mnt/@.snapshots
 umount /mnt
 mount -a rw,noatime,nodiratime,space_cache,ssd,commit=120,compress=zstd,subvol=@ /dev/nvme0n1p2 /mnt
-mount /dev/nvme0n1p1 /mnt/boot
 mkdir -p /mnt/{boot,home,vms,.snapshots,btrfs}
+mount /dev/nvme0n1p1 /mnt/boot
 mount -a rw,noatime,nodiratime,space_cache,ssd,commit=120,compress=zstd,subvol=@home /dev/nvme0n1p2 /mnt/home
 mount -a rw,noatime,nodiratime,space_cache,ssd,commit=120,compress=zstd,subvol=@vms /dev/nvme0n1p2 /mnt/vms
 mount -a rw,noatime,nodiratime,space_cache,ssd,commit=120,compress=zstd,subvol=@.snapshots /dev/nvme0n1p2 /mnt/.snapshots
