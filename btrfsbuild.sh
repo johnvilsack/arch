b66fdc37e21e008cf89a2f0c5cd0276630c2ec4d
John Vilsack <vilsack@gmail.com>
2022-06-10 17:53:09 -0500

    more fixes

--- btrfsbuild.sh
+++ btrfsbuild.sh
@@ -1,8 +1,6 @@
 mount /dev/nvme0n1p2 /mnt
-
-btrfs subvolume create /mnt/@/0/snapshot
 btrfs subvolume create /mnt/@
+mkdir /mnt/@/0
+btrfs subvolume create /mnt/@/0/snapshot
 btrfs subvolume create /mnt/@vm
-btrfs subvolume create /mnt/@.snapshots
-
 umount /mnt
